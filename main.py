a5e46d2e7b53f927dca1d08b615b6154b67cb6a2
Jose Mauro <z.mauro@live.com>
2019-10-15 10:54:28 -0300

    methods to implement save, get and delete operations

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Main(KytosNApp):
 
     @rest('v1/<namespace>/<value>', methods=['POST'])
     @rest('v1/<namespace>/<value>/<timestamp>', methods=['POST'])
-    def save(self, namespace, value, timestamp=None):
+    def rest_save(self, namespace, value, timestamp=None):
         """Save the data in one of the backends."""
         result = self.backend.save(namespace, value, timestamp)
         if result in (400, 404):
@@ -40,7 +40,7 @@ class Main(KytosNApp):
     @rest('v1/<namespace>/start/<start>', methods=['DELETE'])
     @rest('v1/<namespace>/end/<end>', methods=['DELETE'])
     @rest('v1/<namespace>/<start>/<end>', methods=['DELETE'])
-    def delete(self, namespace, start=None, end=None):
+    def rest_delete(self, namespace, start=None, end=None):
         """Delete the data in one of the backends."""
         log.info(start)
         log.info(end)
@@ -60,7 +60,7 @@ class Main(KytosNApp):
           methods=['GET'])
     @rest('v1/<namespace>/<start>/<end>/interpol/<method>/<filter>/<group>',
           methods=['GET'])
-    def get(self, namespace, start=None, end=None, method=None,
+    def rest_get(self, namespace, start=None, end=None, method=None,
             fill=None, group=None):
         """Retrieve the data from one of the backends."""
         result = self.backend.get(namespace, start, end, method, fill, group)
@@ -68,6 +68,55 @@ class Main(KytosNApp):
             return jsonify({"response": 'Not Found'}), 404
 
         return jsonify({"response": result}), 200
+
+    @listen_to('kytos.kronos.save')
+    def event_save(self, event):
+        """Save the data in one of the backends"""
+        try:
+           self.backend.save(event.content['namespace'],
+                             event.content['value'],
+                             event.content['timestamp'])
+        except KeyError:
+            result = None
+            error = None
+        
+        self._execute_callback(event, result, error)
+
+
+    @listen_to('kytos.kronos.get')
+    def event_get(self, event):
+        """Get the data in one of the backends"""
+        try:
+           self.backend.get(event.content['namespace'],
+                            event.content['timestamp'])
+        except KeyError:
+            result = None
+            error = None
+        
+        self._execute_callback(event, result, error)
+
+    @listen_to('kytos.kronos.delete')
+    def event_delete(self, event):
+
+        try:
+           self.backend.delete(event.content['namespace'],
+                               event.content['timestamp'])
+        except KeyError:
+           result = None
+            error = None
+        
+        self._execute_callback(event, result, error)
+    
+    @staticmethod
+    def _execute_callback(event, data, error):
+        """Run the callback function for event calls to the NApp."""
+        try:
+            event.content['callback'](event, data, error)
+        except KeyError:
+            log.error(f'Event {event!r} without callback function!')
+        except TypeError as exception:
+            log.error(f"Bad callback function {event.content['callback']}!")
+            log.error(exception)
 
     def execute(self):
         """Run after the setup method execution.
