229b085fb2ca55eb1ec8a10a03b9e5fe5a70fc80
Jose Mauro <z.mauro@live.com>
2020-02-12 23:38:10 -0300

    test to method rest_save with some adjustments in mock use

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,26 +1,28 @@
 """Tests main module of kytos/kronos."""
-import sys 
 from flask import Flask
-from unittest import TestCase, mock
-
-sys.modules['influxdb'] = mock.MagicMock()
-
-from kytos.core import KytosNApp
-from napps.kytos.kronos.main import Main
 from tests.helpers import get_controller_mock
+
+# pylint: disable=wrong-import-order,wrong-import-position
+import sys
+from unittest import TestCase, mock
+sys.modules['influxdb'] = mock.MagicMock()
+from napps.kytos.kronos.main import Main
+# pylint: enable=wrong-import-order,wrong-import-position
+
 
 class TestMainKronos(TestCase):
     """Class to test kytos/kronos."""
     def setUp(self):
-        self.napp = Main(get_controller_mock())
+        self.napp = Main(get_controller_mock)
 
-    @mock.patch('main.Main.backend')
-    def test_restsave_success(self, mock_main):
+    @mock.patch('napps.kytos.kronos.main.InfluxBackend.save')
+    def test_restsave_success_with_influx(self, mock_main):
         """Test succes in method rest_save."""
         namespace = 'kytos.kronos.telemetry.switches.1.interfaces.232.bytes_in'
         value = '123'
-        
+        timestamp = None
+
         app = Flask(__name__)
         with app.app_context():
-            self.napp.rest_save(namespace, value)
-            mock_main.save.assert_called_with(namespace, value)
+            self.napp.rest_save(namespace, value, timestamp)
+            mock_main.assert_called_with(namespace, value, timestamp)
