996638546e1ee7eeb5b0e00866f16a7c7360524d
jose <z.mauro@live.com>
2021-03-19 10:26:20 -0300

    Update setup.py to make tox fail when tests fail.

--- setup.py
+++ setup.py
@@ -136,6 +136,8 @@ class TestCoverage(Test):
             check_call(cmd, shell=True)
         except CalledProcessError as exc:
             print(exc)
+            print('Coverage tests failed. Fix the errors above and try again.')
+            sys.exit(-1)
 
 
 class Linter(SimpleCommand):
